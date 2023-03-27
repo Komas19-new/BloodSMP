7e7b790cae0a34c88ad91dd6a7651d51e472b5ae
Komas19-new <benoitbourcier@outlook.com>
2023-03-27 16:10:50 +0200

    Release: 1.9.9
    Added: - Easier to implement API to code.
           - Changed text of /bloodver command.
           - Everyone can actually run /bloodver now.
           - Note to builds.json
           - Everyone can run /bloodver, and now it sends a message that only the command runner can see

--- src/main/java/komas19/bloodsmp/plugin/Blocking.java
+++ src/main/java/komas19/bloodsmp/plugin/Blocking.java
@@ -1,6 +1,8 @@
 package komas19.bloodsmp.plugin;
 
 import com.destroystokyo.paper.event.player.PlayerJumpEvent;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -10,7 +12,14 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.inventory.*;
 import org.bukkit.event.player.*;
 import org.bukkit.inventory.ItemStack;
+import komas19.bloodsmp.plugin.Version;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class Blocking extends JavaPlugin implements Listener {
@@ -435,7 +444,27 @@ public class Blocking extends JavaPlugin implements Listener {
             }
         }
     }
-    public void onJoin(PlayerJoinEvent event) {
+    public void onJoin(PlayerJoinEvent event) throws IOException {
+        String jsonS = "";
+        URL url = new URL("https://komas19-new.github.io/BloodSMP/latest-builds.json");
+        URLConnection conn = url.openConnection();
+        conn.connect();
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String inputLine;
+
+        while((inputLine = in.readLine()) != null) {
+            jsonS+=inputLine;
+        }
+        Gson gson = new Gson();
+        JsonObject jsonObject= gson.fromJson(jsonS, JsonObject.class);
+        String versionJson = jsonObject.get("version").getAsString();
+        String version = getServer().getPluginManager().getPlugin("BloodSMP").getPluginMeta().getVersion();
+        in.close();
+
+        if (!versionJson.equals(version)) {
+            Bukkit.broadcastMessage("New version of Blood SMP has released! Current: "+version+ " Newest: "+versionJson);
+        }
+
         ItemStack item = event.getPlayer().getActiveItem();
         if (config.getBoolean("tipped_arrows")) {
             // True
